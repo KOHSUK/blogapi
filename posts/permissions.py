bab0d2ac3feda67a335ef00cebb851d612948847
Kosuke Kihara <ashenden1965@gmail.com>
2020-01-02 12:06:44 +0900

    Chapter7まで終了

---
+++ posts/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+# You should import this class in the views.py and add permission_classes 
+# to use this custom authentication.
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of a post
+        return obj.author == request.user
